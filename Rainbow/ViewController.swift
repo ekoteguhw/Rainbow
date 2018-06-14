d7aa9fd6584bb2a0bec015a4ee997bc71eb71216
Eko Teguh Widodo <id.ekoteguhw@gmail.com>
2018-06-14 12:08:06 +0700

    Initial Commit

---
+++ Rainbow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Eko Teguh Widodo on 14/06/18.
+//  Copyright © 2018 Eko Teguh Widodo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
